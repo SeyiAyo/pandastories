e9699b90f23157e264677f718b6d7b98bcf4b462
Seyi Ayo <oluwaseyiayoola97@gmail.com>
2025-10-24 05:03:15 +0100

    feat: add app variable alias for Vercel deployment compatibility

--- pandastories/wsgi.py
+++ pandastories/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pandastories.settings')
 
 application = get_wsgi_application()
+
+# Vercel expects 'app' variable
+app = application
